ff8872dd4433904118bc16ceee943facaf559ce2
Renan Araujo <araujo2kg@gmail.com>
2024-11-13 17:59:10 -0300

    Finish 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (!Number.isInteger(number1) || number1 < 0) return "ERROR";
+    if (!Number.isInteger(number2) || number2 < 0) return "ERROR";
 
+    let sum = 0;
+    let start = (number1 < number2) ? number1 : number2;
+    let end = (number1 > number2) ? number1 : number2;
+
+    for (start; start <= end; start++){
+        sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
