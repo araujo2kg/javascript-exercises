2d7728839b45a2f6171de6e3e50317044db252a8
Renan Araujo <araujo2kg@gmail.com>
2025-01-09 20:11:54 -0300

    Calculator 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,38 @@
-const add = function() {
-	
+const add = function (x, y, ...opt) {
+    const optional = opt.reduce((total, current) => total + current, 0);
+    return x + y + optional;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+    return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (args) {
+    return args.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (args) {
+    return args.reduce((total, current) => (total *= current));
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+    return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+    let fact = 1;
+    for (let i = x; i > 0; i--) {
+        fact *= i;
+    }
+    return fact;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
