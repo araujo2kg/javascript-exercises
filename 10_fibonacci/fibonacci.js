8637800c2cd365f263ccb9fb0f397eac3852e730
Renan Araujo <araujo2kg@gmail.com>
2025-01-10 18:55:33 -0300

    fibonacci 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+    if (position == 0) return 0;
+    if (position < 0) return "OOPS";
 
+    // let sequence = [1, 1];
+    // for (let i = 1; i < position; i++) {
+    //     sequence.push(sequence[i] + sequence[i - 1]);
+    // }
+    // return sequence[position - 1];
+
+    let previous = 1;
+    let secondPrevious = 0;
+    for (let i = 2; i <= position; i++) {
+        let current = previous + secondPrevious;
+        secondPrevious = previous;
+        previous = current;
+    }
+
+    return previous;
 };
 
 // Do not edit below this line
