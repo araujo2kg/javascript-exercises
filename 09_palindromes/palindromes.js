b42e82e20958897020a8b445291a1dc36f8abafa
Renan Araujo <araujo2kg@gmail.com>
2025-01-10 17:12:26 -0300

    palindromes 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphaString = toAlpha(string);
 
+    let j = 0;
+    for (let i = alphaString.length - 1; i >= 0; i--) {
+        if (alphaString[i] != alphaString[j]) return false;
+        j++;
+    }
+    return true;
+};
+
+const isLetter = function (char) {
+    return char.toLowerCase() != char.toUpperCase();
+};
+
+const isNumber = function (char) {
+    return !isNaN(parseInt(char));
+};
+
+const toAlpha = function (string) {
+    return string
+        .split("")
+        .filter((char) => isLetter(char) || isNumber(char))
+        .join("")
+        .toLowerCase();
 };
 
 // Do not edit below this line
