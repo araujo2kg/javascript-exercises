7081e7ea8360ea56b2bc4e2bba627e4f016299e1
Renan Araujo <araujo2kg@gmail.com>
2025-04-23 20:53:34 -0300

    Wed Apr 23 08:53:40 PM -03 2025

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    return books.reduce((array, currentBook) => {
+        array.push(currentBook.title);
+        return array;
+    }, []);
 };
 
 // Do not edit below this line
